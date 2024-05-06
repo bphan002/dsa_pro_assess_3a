8cf36dabe654ed4739af02211e5a71e093ca4099
Billy <billy.phan4@gmail.com>
2024-05-06 16:46:12 -0700

    problem 2 and multiple choice done

--- 1/problem.py
+++ 1/problem.py
@@ -1,4 +1,17 @@
+from collections import defaultdict
+
 class Solution:
     def countCompleteComponents(self, n, edges):
         # Remove pass and write code here
-        pass
+        #build an adjaceny list
+        graph = defaultdict(list)
+
+
+        for edge in edges:
+            [node1, node2] = edge
+            graph[node1].append(node2)
+            graph[node2].append(node1)
+
+        print(graph)
+        #run dfs on it
+            
